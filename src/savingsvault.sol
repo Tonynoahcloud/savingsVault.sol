a0adeb8f2e43dd2f2d5e1f1cf0cd4ca340b7de26
tonnero234 <tonynoah1@gmail.com>
2025-10-01 16:55:48 +0100

    did fmt and retest

--- src/savingsvault.sol
+++ src/savingsvault.sol
@@ -4,90 +4,53 @@ pragma solidity ^0.8.13;
 contract savingsVault {
     address public owner;
 
-    event deposit(
-        address indexed owner,
-        uint256 amount,
-        uint256 balances
-    );
+    event deposit(address indexed owner, uint256 amount, uint256 balances);
 
-    event goals(
-        address indexed owner,
-        uint256 amount
-    );
-    event withdrawalMade(
-        address indexed owner,
-        uint256 amount,
-        uint256 balances
-    );
+    event goals(address indexed owner, uint256 amount);
+    event withdrawalMade(address indexed owner, uint256 amount, uint256 balances);
 
-    mapping (address => uint256) public balances;
+    mapping(address => uint256) public balances;
 
-    mapping (address => uint256) public savingGoals;
+    mapping(address => uint256) public savingGoals;
 
     uint256 constant PENALTY_FEE = 500;
 
-    constructor () {
+    constructor() {
         owner = msg.sender;
     }
-    
+
     modifier onlyOwner() {
         owner == msg.sender;
         _;
     }
-    
-   
 
-
-   
-    
-
-    function depositEth () public payable {
+    function depositEth() public payable {
         require(savingGoals[msg.sender] >= 1 ether, "you have not set your savings goal"); // made sure a user set a Goal before they can make a deposit
         require(msg.value >= 1 ether, "value has to be positive");
         balances[msg.sender] += msg.value;
-        emit deposit (
-            msg.sender,
-            msg.value,
-            balances[msg.sender]
-        );
+        emit deposit(msg.sender, msg.value, balances[msg.sender]);
     }
-    
-    function setGoal(uint256 amount) public  {
+
+    function setGoal(uint256 amount) public {
         require(amount >= 1 ether, "your set goal must be atleast 1 Eth");
         savingGoals[msg.sender] = amount;
 
-        emit goals (
-            msg.sender,
-            amount
-        );
+        emit goals(msg.sender, amount);
     }
 
-    function checkBalance() public view returns(uint256) {
-        
+    function checkBalance() public view returns (uint256) {
         return balances[msg.sender];
-
     }
 
-    function withdraw (uint256 _amount) public onlyOwner {
-    
-
+    function withdraw(uint256 _amount) public onlyOwner {
         if (balances[msg.sender] >= savingGoals[msg.sender] && balances[msg.sender] > _amount) {
             balances[msg.sender] -= _amount;
-        }
-
-        else if (balances[msg.sender] < savingGoals[msg.sender] && balances[msg.sender] > _amount){
+        } else if (balances[msg.sender] < savingGoals[msg.sender] && balances[msg.sender] > _amount) {
             balances[msg.sender] -= (_amount * PENALTY_FEE) / 10000;
-        }
-        else{
+        } else {
             revert("you dont qualify");
         }
 
-
-        emit withdrawalMade (
-            msg.sender,
-            _amount,
-            balances[msg.sender]
-        );
+        emit withdrawalMade(msg.sender, _amount, balances[msg.sender]);
     }
-    
 }
--- test/savingsvault.t.sol
+++ test/savingsvault.t.sol
@@ -4,19 +4,16 @@ pragma solidity ^0.8.13;
 import {Test} from "forge-std/Test.sol";
 import {savingsVault} from "../src/savingsvault.sol";
 
-
 contract savingsVaultTest is Test {
     savingsVault public savingsvault;
 
-    function setUp() public  {
+    function setUp() public {
         savingsvault = new savingsVault();
-
     }
 
-
     function testdepositEth_GoalSet() public {
-        savingsvault.setGoal (1 ether);
-        savingsvault.depositEth{value:  1 ether}();
+        savingsvault.setGoal(1 ether);
+        savingsvault.depositEth{value: 1 ether}();
         assertGt(savingsvault.savingGoals(address(this)), 0.5 ether);
         assertGt(savingsvault.checkBalance(), 0.5 ether);
     }
@@ -26,9 +23,5 @@ contract savingsVaultTest is Test {
         savingsvault.depositEth{value: 10 ether}();
         savingsvault.withdraw(3 ether); // Withdraw 3 ether
         assertEq(savingsvault.balances(address(this)), 7 ether);
-        
-
     }
-
-
 }
